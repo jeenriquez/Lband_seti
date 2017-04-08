41956983f5ce1930869d11d5196d6032c17d4b79
Emilio Enriquez <jeenriquez@gmail.com>
2017-04-07 17:13:30 -0700

    Replacing use of subproces with os.system.

--- L-band_seti_pipeline.py
+++ L-band_seti_pipeline.py
@@ -6,6 +6,7 @@ import subprocess
 from blimpy import Waterfall
 from optparse import OptionParser
 import sys
+import os
 
 def reset_outs():
     '''
@@ -85,23 +86,36 @@ def cmd_tool(args=None):
 
         command=['python','/home/eenriquez/software/bl-soft/turbo_seti/bin/seti_event.py',star_path+star_name,'-M','2','-s','20','-c','115,395','-o',out_dir+'stars_out/']
         print ' '.join(command)
-        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        (out, err) = proc.communicate()
 
-        with open(out_dir+'logs/'+star_name.replace('.fil','.seti_event.log'), 'a') as f:
-            f.write(out)
+        err_file = out_dir+'logs/'+star_name.replace('.fil','.seti_event.err')
+        out_file = out_dir+'logs/'+star_name.replace('.fil','.seti_event.log')
 
-        if err or proc.returncode != 0:
-            with open(out_dir+'logs/'+star_name.replace('.fil','.seti_event.err'), 'a') as f:
-                f.write(err)
+        try_it = os.system(' '.join(command) +' 2> '+err_file+' 1> '+out_file)
 
-            #------------------------------------
-            #Add failed one to list.
+#         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#         (out, err) = proc.communicate()
+#
+#         with open(out_dir+'logs/'+star_name.replace('.fil','.seti_event.log'), 'a') as f:
+#             f.write(out)
+#
+#         if err or proc.returncode != 0:
+#             with open(out_dir+'logs/'+star_name.replace('.fil','.seti_event.err'), 'a') as f:
+#                 f.write(err)
 
+        #------------------------------------
+        #Add failed one to list.
+
+        if os.path.isfile(err_file):
             error_list = extra_path+out_dir+'L_band_failed_targets.lst'
 
             with open(error_list,'a') as file_fail:
                 file_fail.write(star)
+
+        #------------------------------------
+        #Delete h5 file
+
+        os.remove(out_dir+'stars_out/'+star_name.replace('.fil','.h5'))
+
 
 if __name__ == "__main__":
     cmd_tool()
