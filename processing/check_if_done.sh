9c8aa48f67d7f44f12e251e8436f3af721b9f359
Emilio Enriquez <jeenriquez@gmail.com>
2017-04-12 03:40:41 -0400

    Addding script to check what turbo_seti has fisnished processing.

---
+++ processing/check_if_done.sh
@@ -0,0 +1,2 @@
+grep time: /datax/users/eenriquez/L-band_analysis_try1/logs/*seti_event.log > /datax/users/eenriquez/L-band_analysis/turbo_seti_done_try1_$HOSTNAME.lst
+grep time: /datax/users/eenriquez/L-band_analysis_try2/logs/*seti_event.log > /datax/users/eenriquez/L-band_analysis/turbo_seti_done_try2_$HOSTNAME.lst
